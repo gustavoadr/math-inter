762e050eb6c758721d81c95cdf9aeaf46908f1f9
Gustavo Rocha <gustavo.rocha@nuria.com.br>
2024-06-20 09:20:20 -0300

    template

--- MathInter/Express/Teste.cs
+++ MathInter/Express/Teste.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-namespace GerSegCond.Console.Express;
+namespace Agilis.CodeG;
 
-internal class Teste
+public class Teste
 {
     private const string 
-        fileName = "Express\\Template.txt", 
+        fileName = "CodeG\\Template.txt", 
         outputDir = "C:\\Projetos\\Teste", 
-        inputDir = "C:\\Projetos\\GroupSeguros\\GerSegCond\\Fontes\\GerSegCond.Console";
+        inputDir = "C:\\Projetos\\agilis-util\\Agilis";
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Testes
     public static void Run()
@@ -25,10 +25,11 @@ internal class Teste
         //      Tela
         //      Mapeamento para Barramento
 
-
         // Operações básicas
         Exec("1", "1");
-        //Math("-1", "-1");
+        Exec("-1", "-1");
+        Exec("-.13", "-0,13");
+        Exec(".12", "0,12");
         Exec("1+2", "3");
         Exec("1+2", "3");
         Exec("7*(1+2)", "21");
@@ -41,7 +42,7 @@ internal class Teste
         Exec("f(5, 3)", "-5\r\n3");
         Exec("f((1+1)*2, (1+(2))*(1+3))", "-4\r\n12");
         // Mais de uma operação em uma expressão
-        Exec("1;2", "1\r\n2");
+        Exec("1;-.2", "1\r\n-0,2");
         // Booleano e outros operadores
         Exec("1==2", "False");
         Exec("1>2", "False");
@@ -61,51 +62,52 @@ internal class Teste
         Exec("MODEL.passeios", null, null, new Viagem());
         Exec("MODEL.passeios.Count", "10", null, new Viagem());
         Exec("MODEL.passeios().Count()", "10", null, new Viagem());
+        Exec("MODEL.passeios[2].titulo[0]", "T", null, new Viagem());
         // Variavel
         Exec("i=1+2; i; \"i\"", "3\r\ni");
         Exec("i=1; i=i+1; i; \"i\"", "2\r\ni");
         Exec("i=1; i=\"i\"+i; i", "i1");
         // For
-        Exec("for(i,0,3,i)", "0\r\n1\r\n2");
-        Exec("for(i,0+1-1,1+2*2-2,i)", "0\r\n1\r\n2");
-        Exec("a=0;b=3;for(i,a,b,i)", "0\r\n1\r\n2");
-        Exec("a=0;b=3;for(i,a,b,((i+1));(i))", "1\r\n0\r\n2\r\n1\r\n3\r\n2");
+        Exec("for(i,0,3,{i})", "0\r\n1\r\n2");
+        Exec("for(i,0+1-1,1+2*2-2,{i})", "0\r\n1\r\n2");
+        Exec("a=0;b=3;for(i,a,b,{i})", "0\r\n1\r\n2");
+        Exec("a=0;b=3;for(i,a,b,{((i+1));(i)})", "1\r\n0\r\n2\r\n1\r\n3\r\n2");
         // If
-        Exec("i=\"a\";if(1==1, i, i+1)", "a");
-        Exec("i=\"a\";if(1!=1, i, i+1)", "a1");
+        Exec("i=\"a\";if(1==1, {i}, {i+1})", "a");
+        Exec("i=\"a\";if(1!=1, {i}, {i+1})", "a1");
         // replace => troca o conteúdo dentro de uma string se existir variável com mesmo nome
-        Exec($"a=123;replace(\"casa\")", "c123s123");
-        Exec($"a=123;s=\"bbb\";replace(\"casa\")", "c123bbb123");
-        Exec($"for(i,0,2,replace(\"Guilherme\"))", "Gu0lherme\r\nGu1lherme");
-        // saveFile
+        Exec("a=123;replace(\"casa\")", "c123s123");
+        Exec("a=123;s=\"bbb\";replace(\"casa\")", "c123bbb123");
+        Exec("for(i,0,2,{replace(\"Guilherme\")})", "Gu0lherme\r\nGu1lherme");
+        // save
         if (Directory.Exists(outputDir))
             Directory.Delete(outputDir, true);
-        Exec($"saveFile(\"{outputDir}\\{fileName}\", 1)", "1", fileName);
-        Exec($"saveFile(\"{outputDir}\\{fileName}\", \"1\")", "1", fileName);
-        Exec($"Template=\"MinhaClasse\";saveFile(\"{outputDir}\\\"+replace(\"{fileName}\"), for(i,0,4,i+1))", "1234", "Express\\MinhaClasse.txt");
+        Exec($"save(\"{outputDir}\\{fileName}\", {{{"1"}}})", "1", fileName);
+        Exec($"Template=\"MinhaClasse\";save(\"{outputDir}\\\"+replace(\"{fileName}\"), {{for(i,0,4,{{i+1}})}})", "1234", "CodeG\\MinhaClasse.txt");
+        // function
+        Exec($"function(soma, \"a\", \"b\", {{a+b}}); soma(2,3);", "5");
+        Exec($"function(primeiroEsoma, \"a\", \"b\", {{a;a+b}}); primeiroEsoma(2,3);", "2\r\n5");
+        Exec("function(soma, \"a\", \"b\", {a+b;});soma(1, soma(1, 2));", "4");
 
         // simples
-        GerarTemplate("Express\\Template1.txt", "1");
+        GerarTemplate("CodeG\\Template01.txt", "1");
         // trocando o nome do arquivo
-        GerarTemplate("Express\\Template2.txt", "Template=Macarrao", "Express\\Macarrao2.txt");
+        GerarTemplate("CodeG\\Template02.txt", "Template=Macarrao", "CodeG\\Macarrao02.txt");
         // for e if
-        GerarTemplate("Express\\Template3.txt", "\tSalles\r\n\tSalles\r\n\tGuilherme\r\n\tSalles\r\n\tSalles\r\n\tSalles\r\n");
+        GerarTemplate("CodeG\\Template03.txt", "\tSalles\r\n\tSalles\r\n\tGuilherme\r\n\tSalles\r\n\tSalles\r\n\tSalles\r\n");
         // expressao no meio do arquivo
-        GerarTemplate("Express\\Template4.txt");
+        GerarTemplate("CodeG\\Template04.txt", "\tSalles abcdefghijklmnopqrztuvxz[0]=a\r\n\tSalles abcdefghijklmnopqrztuvxz[1]=b\r\n\tGuilherme abcdefghijklmnopqrztuvxz[2]=c\r\n\tSalles abcdefghijklmnopqrztuvxz[3]=d\r\n\tSalles abcdefghijklmnopqrztuvxz[4]=e\r\n\tSalles abcdefghijklmnopqrztuvxz[5]=f\r\n");
         // gerando arquivos dentro do loop
-        GerarTemplate("Express\\Template5.txt");
+        GerarTemplate("CodeG\\Template05.txt", "\t2: Guilherme\r\n", "CodeG\\Template05-2.txt");
         // keep simples e keep variando o nome
-        GerarTemplate("Express\\Template6.txt");
-        GerarTemplate("Express\\Template7.txt");
+        GerarTemplate("CodeG\\Template06.txt");
+        GerarTemplate("CodeG\\Template07.txt");
         // gerando com modelo
-        GerarTemplate("Express\\Template8.txt", null, null, new Viagem());
-        GerarTemplate("Express\\Template9.txt", null, null, new Viagem());
-
-        //ajustar operação com valor negativo -1 (tokenize)
-        //ajustar reflection (primeira funcao que bate) (tokenize, antes do param coloca o num de argumento) (toString de data com formato, template 9)
-        //reflection (testes linha 62, 63) ajustar local
-        //diferenciar variavel local de variavel global
-        //implementar função (declarar a função e executar a função declarada)
+        GerarTemplate("CodeG\\Template08.txt", null, null, new Viagem());
+        GerarTemplate("CodeG\\Template09.txt", null, null, new Viagem());
+        // gerando com função
+        GerarTemplate("CodeG\\Template10.txt", "3");
+        GerarTemplate("CodeG\\Template11.txt", "c");
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// 
@@ -140,7 +142,7 @@ internal class Teste
     {
         return (-x, y);
     }
-    private static Stack<object> ReflexaoR2(Stack<object> stack)
+    private static Stack<object> ReflexaoR2(int nParam, Stack<object> stack)
     {
         double y = (double)stack.Pop();
         double x = (double)stack.Pop();
@@ -150,14 +152,15 @@ internal class Teste
         ret.Push(refletido.y);
         return ret;
     }
-    private static void Exec(string expression, string resposta = null, string arquivo = null, object MODEL = null)
+    private static void Exec(string expression, string resposta = null, string arquivo = null, object obj = null)
     {
         ExpressionBase ex = new ExpressionCodeGenerator();
-        ex.addFunction("sin", (Stack<object> stack) => System.Math.Sin((double)stack.Pop()));
+        ex.addFunction("sin", (int nParam, Stack<object> stack) => System.Math.Sin((double)stack.Pop()));
         ex.addFunction("f", ReflexaoR2);
         ex.addVar("PI", Math.PI);
-        if (MODEL != null) 
-            ex.addVar("MODEL", MODEL);
+        if (obj != null) 
+            ex.addVar("MODEL", obj);
+        Debug.WriteLine(expression);
         ex.parse(expression);
         Debug.WriteLine(ex.convertToRPN());
         string v = ex.evaluateS("\r\n");
